ecab0d1a833c29c3e0e8837b892c3084feb751a4
Maycon Benito <mayki17321@gmail.com>
2020-03-24 10:26:46 -0300

    add meta description

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,10 @@ export default function Index() {
     <div>
       <Head>
         <title>Shortfy</title>
+        <meta
+          name="description"
+          content="Shortfy.ml is a url shortener to reduce long link. Use our tool to Shortfy links and share them."
+        />
         <script
           data-ad-client="ca-pub-6005708002042517"
           async
