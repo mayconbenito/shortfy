8c20eab405882333bab6dac0d89858403f1c2879
Maycon Benito <mayki17321@gmail.com>
2020-06-29 00:27:26 -0300

    change head tag

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,19 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <title>Shortfy - The easiest to use url shortener</title>
+          <meta
+            name="description"
+            content="Shortfy.ml is a url shortener to reduce long link. Use our tool to Shortfy links and share them."
+          />
+
+          <script
+            data-ad-client="ca-pub-6005708002042517"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -35,19 +35,6 @@ export default function Index() {
 
   return (
     <div>
-      <Head>
-        <title>Shortfy - The easiest to use url shortener</title>
-        <meta
-          name="description"
-          content="Shortfy.ml is a url shortener to reduce long link. Use our tool to Shortfy links and share them."
-        />
-
-        <script
-          data-ad-client="ca-pub-6005708002042517"
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        ></script>
-      </Head>
       <h1>Shortfy</h1>
       <form className="form" onSubmit={handleShortfyURL}>
         <label htmlFor="link">Type the link you want to Shortify</label>
